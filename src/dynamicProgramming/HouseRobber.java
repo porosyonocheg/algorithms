ea56a975d14ee3af9437016e48a38ff459eb4180
Sergey Shershavin <mourinho89@rambler.ru>
2021-07-19 15:01:21 +0300

    int robInRound(int[] nums) added in HouseRobber.class

--- src/dynamicProgramming/HouseRobber.java
+++ src/dynamicProgramming/HouseRobber.java
@@ -5,6 +5,8 @@ package dynamicProgramming;
  * @author Сергей Шершавин*/
 
 public class HouseRobber {
+
+    /**Дома расставлены последовательно в ряд друг за другом*/
     public int rob(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
         if (nums.length == 1) return nums[0];
@@ -19,5 +21,21 @@ public class HouseRobber {
             sum[i] = Math.max(sum[i-2] + nums[i], sum[i-1]); // макс. сумма для текущего дома: либо макс. сумма, собранная к предыдущему дому, либо к предпредыдущему + сумма в данном доме
         }
         return sum[nums.length-1];
+    }
+
+    /**Дома расставлены в круг (последний является соседним с первым)*/
+    public int robInRound(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        return Math.max(robInRound(nums, 0, nums.length-2), robInRound(nums, 1, nums.length-1));
+    }
+
+    private int robInRound(int[] nums, int start, int end) {
+        int rob = 0, skip = 0;
+        for (int i = start; i <= end; i++) {
+            int prevRob = rob, prevSkip = skip;
+            rob = skip + nums[i];
+            skip = Math.max(prevRob, prevSkip);
+        }
+        return Math.max(rob, skip);
     }
 }
