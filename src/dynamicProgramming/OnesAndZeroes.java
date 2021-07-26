320292e2b62139d3ce35208274c83235853668a3
Sergey Shershavin <mourinho89@rambler.ru>
2021-07-26 13:25:15 +0300

    OnesAndZeroes.class added in dynamicProgramming package

---
+++ src/dynamicProgramming/OnesAndZeroes.java
@@ -0,0 +1,23 @@
+package dynamicProgramming;
+
+/**
+ * @author Сергей Шершавин*/
+
+public class OnesAndZeroes {
+    public int findMaxForm(String[] strs, int m, int n) {
+        int[][] dp = new int[m+1][n+1]; //dp[i][j] хранит максимальное число строк, которое содержит i-нулей и j-единиц.
+        for (String s : strs) {
+            int countOnes = 0, countZeroes = 0;
+            for (char c : s.toCharArray()) {
+                if (c == '1') countOnes++;
+                else countZeroes++;
+            }
+            for (int i = m; i >= countZeroes; i--) {
+                for (int j = n; j >= countOnes; j--) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i-countZeroes][j-countOnes] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
