d7ea07a5d3adceb81fd5d42f83a8cbf511a389b3
Sergey Shershavin <mourinho89@rambler.ru>
2021-08-20 14:42:11 +0300

    public int maxProfit(int[] prices, int transactions) edited, bag in ShellSort.class fixed

--- src/arraySorting/ShellSort.java
+++ src/arraySorting/ShellSort.java
@@ -8,6 +8,12 @@ public class ShellSort implements Sorting {
 
     @Override
     public void sort(int[] array) {
+        if (array.length == 2 && array[0] > array[1]) {
+            int temp = array[1];
+            array[1] = array[0];
+            array[0] = temp;
+            return;
+        }
         int step = 1;
         while (step < array.length) step <<= 1;
         step >>= 1;
--- src/dynamicProgramming/BestTimeToBuyAndSellStock.java
+++ src/dynamicProgramming/BestTimeToBuyAndSellStock.java
@@ -1,5 +1,7 @@
 package dynamicProgramming;
 
+import java.util.Arrays;
+
 /** Максимализация прибыли от разницы цен покупки-продажи акций. Единовременно доступна только одна транзакция: перед
  * очередной покупкой предыдущий пакет должен быть продан.
  * @author Сергей Шершавин*/
@@ -10,7 +12,9 @@ public class BestTimeToBuyAndSellStock {
      * @param transactions количество доступных транзакций (покупка-продажа)
      * @return максимальную прибыль*/
     public int maxProfit(int[] prices, int transactions) {
+        if (transactions < 1) throw new IllegalArgumentException("The number of transactions must be positive!");
         if (transactions == 1) return oneTransaction(prices);
+        if (transactions < prices.length / 2) return moreThanOneTransaction(prices, transactions);
         return infinityTransactions(prices);
     }
 
@@ -26,6 +30,20 @@ public class BestTimeToBuyAndSellStock {
             }
         }
         return max;
+    }
+
+    private int moreThanOneTransaction(int[] prices, int k) {
+        int[] emptyHands = new int[k+1]; //emptyHands[i] - макс. прибыль после продажи акций для i-ой транзакции
+        int[] withStocks = new int[k+1]; //withStocks[i] - макс. сумма после покупки акций для i-ой транзакции
+        Arrays.fill(withStocks, Integer.MIN_VALUE);
+
+        for (int price : prices) {
+            for (int j = k; j >= 1; j--) {
+                emptyHands[j] = Math.max(emptyHands[j], withStocks[j] + price);
+                withStocks[j] = Math.max(withStocks[j], emptyHands[j - 1] - price);
+            }
+        }
+            return emptyHands[k];
     }
 
     private int infinityTransactions(int[] prices) {
